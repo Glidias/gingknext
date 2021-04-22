b10dff33de6379e5d0d22a5e872db85282882f2e
Glidias <glidias@hotmail.com>
2021-04-22 17:58:50 +0800

    preparing doc viewer

---
+++ shared/util/doc-helpers.js
@@ -0,0 +1,333 @@
+const _ = require("lodash");
+const { TweenMax } = require("gsap");
+
+/* ==== Utility functions ===== */
+
+function toHex(s) {
+  // utf8 to latin1
+  var s = unescape(encodeURIComponent(s));
+  var h = "";
+  for (var i = 0; i < s.length; i++) {
+    h += s.charCodeAt(i).toString(16);
+  }
+  return h;
+}
+
+/* ===== DOM Manipulation ===== */
+
+var setTextarea = (m, f) => {
+  if (m.viewState.editing !== null && f !== null) {
+    var textarea = document.getElementById("card-edit-" + m.viewState.editing);
+    textarea.value = f;
+  }
+};
+
+var scrollHorizontal = (colIdx, instant) => {
+  lastColumnIdx = colIdx;
+  _.delay(scrollHorizTo, 20, colIdx, instant);
+};
+
+var scrollColumns = (scrollInfo) => {
+  scrollInfo.columns.map(column => {
+    let positionParam;
+    switch (column.scrollData.position) {
+      case "Center":
+        positionParam = "center";
+        break;
+
+      case "After":
+        positionParam = "bottom";
+        break;
+
+      case "Before":
+        positionParam = "top";
+        break;
+
+      case "Between":
+        positionParam = "bottom"
+        break;
+    }
+    _.delay(scrollTo, 20, column.scrollData.target, column.columnIdx, scrollInfo.instant, positionParam);
+  });
+}
+
+var scrollFullscreen = function (cid) {
+  _.delay(scrollFullscreenTo, 20, cid);
+}
+
+var scrollFullscreenTo = function (cid) {
+  var card = document.getElementById("card-" + cid.toString());
+  var col = document.getElementById("fullscreen-main");
+  if (card == null) {
+    console.log("scroll error: not found", cid);
+    return;
+  }
+  var rect = card.getBoundingClientRect();
+
+  TweenMax.to(col,  0.25, {
+    scrollTop:
+      col.scrollTop + (rect.top + rect.height * 0.5 - col.offsetHeight * 0.5),
+    ease: Power2.easeInOut,
+  });
+}
+
+var scrollTo = function (cid, colIdx, instant, position) {
+  var card = document.getElementById("card-" + cid.toString());
+  var col = document.getElementsByClassName("column")[colIdx - 1];
+  let doc = document.getElementById("document");
+  if (card == null || doc == null) {
+    console.log("scroll error: not found", cid);
+    return;
+  }
+  let docRect = doc.getBoundingClientRect();
+  var rect = card.getBoundingClientRect();
+  let positionMultiplier = position === "top" ? 0 : position === "center" ? 0.5 : 1;
+
+  TweenMax.to(col, instant ? 0 : 0.25, {
+    scrollTop:
+      col.scrollTop + (rect.top + rect.height * positionMultiplier - (docRect.top+docRect.bottom)*0.5),
+    ease: Power2.easeInOut,
+  });
+};
+
+var scrollHorizTo = function (colIdx, instant) {
+  let scrollDuration = instant ? 0 : 0.3;
+  var col = document.getElementsByClassName("column")[colIdx - 1];
+  var appEl = document.getElementById("document");
+
+
+  let scrollDoneCallback = () => {};
+  if (!instant) {
+    /* Remove CSS scroll-snap behavior, otherwise won't animate */
+    appEl.classList.add("scroll-animation");
+    scrollDoneCallback = () => { appEl.classList.remove("scroll-animation") }
+  }
+
+  if (col == null) {
+    console.log("scroll horiz error: not found", colIdx);
+    return;
+  }
+  TweenMax.to(appEl, scrollDuration, {
+    scrollLeft: col.offsetLeft + 0.5  * (col.offsetWidth - appEl.offsetWidth),
+    ease: Power2.easeInOut,
+    onComplete: scrollDoneCallback
+  });
+};
+
+/* ===== Fillet Calculations ===== */
+
+// List (Html) -> List (Maybe {top: Int, bottom: Int})
+const getFilletData = (columnNodes) => {
+  let getColumnTopBottomInfo = (col) => {
+    let activeCards = col.getElementsByClassName("card active");
+    let activeGroups = col.getElementsByClassName("group active-descendant");
+    if (activeCards.length > 0) {
+      let activeRect = activeCards[0].getBoundingClientRect();
+      return {top: activeRect.top, bottom: activeRect.bottom};
+    } else if (activeGroups.length > 0) {
+      let firstActiveGroupRect = activeGroups[0].getBoundingClientRect();
+      let lastActiveGroupRect = activeGroups[activeGroups.length - 1].getBoundingClientRect();
+      return {top: firstActiveGroupRect.top, bottom: lastActiveGroupRect.bottom};
+    } else {
+      return null;
+    }
+  };
+
+  return columnNodes.map(getColumnTopBottomInfo)
+};
+
+const setColumnFillets = (currColumn, colIdx, filletData) => {
+  if(filletData[colIdx-1] == null && filletData[colIdx] != null && filletData[colIdx + 1] != null) {
+    // Active Card
+    let activeCard = currColumn.getElementsByClassName("card active")[0];
+    if (activeCard) {
+      let topDelta = Math.min(Math.max((filletData[colIdx].top - filletData[colIdx+1].top)*0.5, -16), 16);
+      let bottomDelta = Math.min(Math.max((filletData[colIdx].bottom - filletData[colIdx+1].bottom)*0.5, -16), 16);
+      let [filletTop, filletBottom] = activeCard.getElementsByClassName("fillet");
+
+      if (filletTop && filletBottom) {
+        filletTop.style.display = "block";
+        filletBottom.style.display = "block";
+        setTop(topDelta, filletTop);
+        setBottom(bottomDelta, filletBottom);
+      }
+    }
+
+  } else if(filletData[colIdx] != null && filletData[colIdx-1] != null) {
+    // Active Descendant
+    let topLeftDelta = Math.min(Math.max((filletData[colIdx].top - filletData[colIdx-1].top)*0.5, -16), 16);
+    let bottomLeftDelta = Math.min(Math.max(filletData[colIdx].bottom - filletData[colIdx-1].bottom, -16), 16);
+    let allTopLeftFillets = currColumn.getElementsByClassName("fillet top-left");
+    let allBottomLeftFillets= currColumn.getElementsByClassName("fillet bottom-left");
+    let filletTopLeft = allTopLeftFillets[0];
+    let filletBottomLeft = allBottomLeftFillets[allBottomLeftFillets.length - 1];
+
+    if(filletTopLeft && filletBottomLeft) {
+      filletTopLeft.style.display = "block";
+      filletBottomLeft.style.display = "block";
+      Array.from(allTopLeftFillets).map((f,i) => {if (i!=0) {f.style.display = "none";}})
+      Array.from(allBottomLeftFillets).map((f,i,a) => {if (i != a.length - 1) {f.style.display = "none";}})
+
+      setTop(topLeftDelta, filletTopLeft);
+      setBottom(bottomLeftDelta, filletBottomLeft);
+    }
+
+    // Active Descendant Right (Nested If)
+    let filletTopRight = currColumn.getElementsByClassName("fillet top-right")[0];
+    let allBottomRightFillets= currColumn.getElementsByClassName("fillet bottom-right");
+    let filletBottomRight = allBottomRightFillets[allBottomRightFillets.length - 1];
+    if (filletTopRight && filletBottomRight) {
+      if (filletData[colIdx + 1] != null) {
+        filletTopRight.style.display = "block";
+        filletBottomRight.style.display = "block";
+        let topRightDelta = Math.min(Math.max((filletData[colIdx].top - filletData[colIdx+1].top)*0.5, -16), 16);
+        let bottomRightDelta = Math.min(Math.max(filletData[colIdx].bottom - filletData[colIdx+1].bottom, -16), 16);
+
+        setTop(topRightDelta, filletTopRight);
+        setBottom(bottomRightDelta, filletBottomRight);
+      } else {
+        filletTopRight.style.display = "none";
+        filletBottomRight.style.display = "none";
+      }
+    }
+  }
+}
+
+const setTop = (delta, el) => {
+  if (delta > 0) {
+    el.style.height = delta + "px";
+    el.style.top = "-" + delta + "px";
+    el.classList.remove("flipped");
+  } else {
+    el.style.height = -delta + "px";
+    el.style.top = 0;
+    el.classList.add("flipped");
+  }
+}
+
+const setBottom = (delta, el) => {
+  if (delta > 0) {
+    el.style.height = delta + "px";
+    el.style.bottom = 0;
+    el.classList.add("flipped");
+  } else {
+    el.style.height = -delta + "px";
+    el.style.bottom = delta + "px";
+    el.classList.remove("flipped");
+  }
+}
+
+/* ===== Shared variables ===== */
+
+const errorAlert = (title, msg, err) => {
+  return {
+    title: title,
+    message: msg,
+    detail: err.message.split("\n")[0],
+    type: "error",
+    buttons: ["OK"],
+  };
+};
+
+var shortcuts = [
+  "shift+enter",
+  "mod+enter",
+  "mod+s",
+  "enter",
+  "esc",
+  "mod+backspace",
+  "mod+j",
+  "mod+down",
+  "mod+k",
+  "mod+up",
+  "mod+l",
+  "mod+right",
+  "mod+shift+j",
+  "mod+shift+down",
+  "mod+shift+k",
+  "mod+shift+up",
+  "h",
+  "j",
+  "k",
+  "l",
+  "left",
+  "down",
+  "up",
+  "right",
+  "alt+left",
+  "alt+h",
+  "alt+down",
+  "alt+j",
+  "alt+up",
+  "alt+k",
+  "alt+right",
+  "alt+l",
+  "alt+shift+up",
+  "alt+shift+down",
+  "alt+home",
+  "alt+end",
+  "alt+0",
+  "alt+1",
+  "alt+2",
+  "alt+3",
+  "alt+4",
+  "alt+5",
+  "alt+6",
+  "[",
+  "]",
+  "mod+o",
+  "mod+x",
+  "mod+c",
+  "mod+v",
+  "mod+shift+v",
+  "mod+z",
+  "mod+shift+z",
+  "mod+r",
+  "mod+b",
+  "mod+i",
+  "end",
+  "home",
+  "pageup",
+  "pagedown",
+  "/",
+  "w",
+];
+
+var needOverride = [
+  "shift+enter",
+  "mod+s",
+  "mod+n",
+  "mod+o",
+  "mod+shift+s",
+  "mod+h",
+  "mod+j",
+  "mod+k",
+  "mod+l",
+  "mod+b",
+  "mod+i",
+  "alt+0",
+  "alt+1",
+  "alt+2",
+  "alt+3",
+  "alt+4",
+  "alt+5",
+  "alt+6",
+];
+
+if (window.navigator.platform.toUpperCase().indexOf('MAC') < 0 ) {
+  needOverride.push("alt+left");
+}
+
+/* ===== CommonJS Module exports ===== */
+
+module.exports = {
+  scrollHorizontal: scrollHorizontal,
+  scrollColumns: scrollColumns,
+  scrollFullscreen: scrollFullscreen,
+  getFilletData: getFilletData,
+  setColumnFillets: setColumnFillets,
+  errorAlert: errorAlert,
+  shortcuts: shortcuts,
+  needOverride: needOverride,
+  toHex: toHex,
+};
--- shared/util/gingko.ts
+++ shared/util/gingko.ts
@@ -1,46 +1,72 @@
-import { curly } from "node-libcurl";
+import { DOMAIN } from '../constants';
 
 export interface GingkoNode {
 	content:string,
-	key:string,
+  _id:string,
 	children?:GingkoNode[]
 }
 
+/* // exportable?
+export interface GingkoNodeFull extends GingkoNode {
+  //"_id": "799a13d49dda2e94db0001b5",
+  //"treeId": "799a135f9dda2e94db0001b2",
+  //"seq": 10140691,
+  //"position": 1,
+  //"parentId": "799a136d9dda2e94db0001b4",
+  //"deleted": true
+
+  //seq: number,
+  //position
+}
+*/
+
 export type GingkoTree = GingkoNode[];
 
-let valueKeyCounter = 0;
+var valueKeyCounter = 0;
 
 export async function loadGingkoTree(treeid:string, cleanup:boolean=true):Promise<GingkoTree | null> {
   let errorCode = 0;
-  let result:GingkoTree = [];
+  let result:GingkoTree | null = null;
+  const { curly } = require("node-libcurl");
 
   try {
     const curlOpts = {
       httpHeader: ['Content-type: application/json'],
       SSL_VERIFYPEER: 0
     };
+    let resultData;
+    let isMock = treeid.charAt(0) === "-";
+    const { statusCode, data, headers } = await curly.get(
+      !isMock ? 'https://gingkoapp.com/' + treeid + '.json'
+      : DOMAIN + '/mockdata/'+treeid.slice(1) + '.json'
+    , curlOpts);
 
-    const { statusCode, data, headers } = await curly.get('https://gingkoapp.com/' + treeid + '.json', curlOpts);
-    if (!Array.isArray(data)) {
+    resultData = data;
+
+
+    if (!Array.isArray(resultData)) {
       errorCode |= 1;
     }
-    else result = Array.isArray(data) ? data : [];
+    else result = resultData;
   } catch(err) {
-    // if (IS_DEV) console.log(err);
+    if (process.env.NODE_ENV === "development") {
+      console.error(err);
+    }
     errorCode |= 2;
   }
   // to-consider: error report if any
-  if (result.length && cleanup) {
+  if (result && result.length && cleanup) {
     result = cleanupGingkoTree(result);
   }
   return result;
 }
 
-export function cleanupGingkoTree(tree:GingkoNode[]):GingkoTree {
+export function cleanupGingkoTree(tree:GingkoTree):GingkoTree {
   valueKeyCounter = 0;
+  tree = tree.filter(nodeGotContent);
   for (let i = 0, l = tree.length; i < l; i++) {
     setupNode(tree[i]);
-    cleanNode(tree[i]);
+    cleanNodeChildren(tree[i]);
   }
   return tree;
 }
@@ -49,21 +75,40 @@ function nodeGotContent(node:GingkoNode):boolean {
   return node.content != "";
 }
 
-function cleanNodes(nodes:GingkoNode[]):GingkoTree {
-  return nodes.filter(nodeGotContent);
-}
-
 function setupNode(node:GingkoNode):void {
-  node.key =  "_" + (valueKeyCounter++);
+  if (!node._id) node._id =  "_" + (valueKeyCounter++);
 }
 
-function cleanNode(node:GingkoNode):void {
+function cleanNodeChildren(node:GingkoNode):void {
   if (!node.children) return;
   if ( node.children.length > 0) {
-    node.children = cleanNodes(node.children);
+    node.children = node.children.filter(nodeGotContent);
   }
   for (let i = 0, l = node.children.length; i < l; i++) {
     setupNode(node.children[i]);
-    cleanNode(node.children[i]);
+    cleanNodeChildren(node.children[i]);
   }
+}
+
+// -- https://github.com/gingko/client/blob/95a3461d0648f21da49ab5c63b1a0365a3e9256a/src/elm/Doc/TreeUtils.elm
+
+// getChildren~, getDescendants, getColumns!, getParent, getAncestors  ,
+
+export function getColumns(tree:GingkoTree):GingkoNode[][] {
+  let columns:GingkoNode[][] = [];
+  // push root level =1 of tree
+  columns.push(tree);
+  // push level >c(=1) of tree
+  let curLevelTree:GingkoNode[] = tree;
+  while (curLevelTree.length > 0) {
+    let collector:GingkoNode[] = [];
+    curLevelTree.forEach((n)=> {
+      if (n.children && n.children.length) {
+        collector.push(...n.children);
+      }
+    });
+    if (collector.length) columns.push(collector);
+    curLevelTree = collector;
+  }
+  return columns;
 }
