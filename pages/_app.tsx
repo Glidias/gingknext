a9ad0869f63b0a6266cb654e364e6f63a6a0e447
Glidias <glidias@hotmail.com>
2021-04-26 09:35:43 +0800

    missing meta nozoom settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <div id="app-root">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width,initial-scale:1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
         <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400;1,700&amp;family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&amp;family=Roboto+Mono:wght@400;700&amp;display=swap" rel="stylesheet"/>
         <link rel="stylesheet" href="/shared.css"/>
         <link rel="stylesheet" href="/style.css"/>
