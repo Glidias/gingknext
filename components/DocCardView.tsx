e54a773bf547576e84e543c7f3eb5ef490aff822
Glidias <glidias@hotmail.com>
2021-04-26 16:44:33 +0800

    alternate href link processing

--- components/DocCardView.tsx
+++ components/DocCardView.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, memo, useEffect, useRef } from "react"
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw';
+import { ATTR_DOMAIN_SLICE } from "../shared/constants";
 /* // to consider for future releases
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
@@ -13,6 +14,9 @@ const DocCardView: FunctionComponent<{content: string}> = memo((props) => {
     if (containerRef && containerRef.current) {
       containerRef.current.querySelectorAll('a').forEach((a)=>{
         if (!a.hasAttribute('target')) a.target = "_blank";
+        if (a.rel === 'alternate') {
+          processAlt(a);
+        }
       });
     }
 	}, [props.content])
@@ -25,5 +29,18 @@ const DocCardView: FunctionComponent<{content: string}> = memo((props) => {
 	}, (prevProps, props) => {
 		return prevProps.content !== props.content;
 });
+
+function processAlt(a:HTMLAnchorElement) {
+  let urlChk;
+  let theUrl;
+  if (a.hasAttribute(ATTR_DOMAIN_SLICE)) {
+    urlChk = window.location.protocol.concat("//").concat(window.location.host);
+    theUrl = a.href.slice(0, urlChk.length);
+  } else {
+    urlChk = window.location.href;
+    theUrl = a.href;
+  }
+  if (urlChk === theUrl) a.removeAttribute('href');
+}
 
 export default DocCardView;
